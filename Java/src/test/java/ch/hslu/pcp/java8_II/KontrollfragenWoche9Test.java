f0fb6e34284530930bfe9dea6750c365a2f78277
Ruedi Arnold <ruedi.arnold@hslu.ch>
2017-11-08 21:20:40 +0100

    Added correct header.

--- Java/src/test/java/ch/hslu/pcp/java8_II/KontrollfragenWoche9Test.java
+++ Java/src/test/java/ch/hslu/pcp/java8_II/KontrollfragenWoche9Test.java
@@ -1,8 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * HSLU I
+ * Module PCP
+ * ruedi.arnold@hslu.ch
  */
+
 package ch.hslu.pcp.java8_II;
 
 import java.util.Comparator;
